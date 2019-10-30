95dbc5320752a4d5130ba819404b83b67c35b700
wangruichens <18049863349@163.com>
2019-10-30 17:47:16 +0800

    leetcode

--- 单调栈.py
+++ 单调栈.py
@@ -6,14 +6,27 @@ class Solution(object):
         :rtype: int
         """
 
-        tmp = []
+        stack = []
         heights = [0] + heights + [0]
         area = 0
         for i, v in enumerate(heights):
-            while tmp and heights[tmp[-1]] > v:
-                idx = tmp.pop()
+            while stack and heights[stack[-1]] > v:
+                idx = stack.pop()
                 h = heights[idx]
-                area = max(area, (i - tmp[-1] - 1) * h)
-            tmp.append(i)
+                area = max(area, (i - stack[-1] - 1) * h)
+            stack.append(i)
 
         return area
+
+
+# leetcode 739
+class Solution(object):
+    def dailyTemperatures(self, T):
+        stack = []
+        res = [0] * len(T)
+        for i, v in enumerate(T):
+            while stack and T[stack[-1]] < v:
+                pos = stack.pop()
+                res[pos] = i - pos
+            stack.append(i)
+        return res
